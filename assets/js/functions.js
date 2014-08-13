c2b58154334291d96908619311ac5397264a3344
wushuyi <wushuyi541@gmail.com>
2014-08-13 14:14:14 +0800

    Signed-off-by: wushuyi <wushuyi541@gmail.com>

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,23 +1,55 @@
-(function($){
+;(function(fun, global) {
+	var pgFun = {};
+	var $cache = {};
+	var pgScroll = [];
+	pgFun.pubScroll = function() {
+		pgScroll[0] = new IScroll('.wrapper', {
+			click : true,
+			bounce : false
+		});
+		pgScroll[1] = new IScroll('#pop', {
+			click : true,
+			bounce : false
+		});
+	};
+	pgFun.index = function() {
+		this.pubScroll();
+		$cache.yxgzBtn = $('.yxgzBtn');
+		$cache.pop = $('#pop');
+		$cache.infoCent = $('.info .js .cent');
+		$cache.popHdxq = $('.popHdxq', $cache.pop);
+		$cache.popHdxqX = $('.popHdxqX', $cache.popHdxqX);
+		console.log($cache.yxgzBtn);
+		$cache.yxgzBtn.on('click', function(e) {
+			e.stopPropagation();
+			e.preventDefault();
+			$cache.pop.show();
+			$cache.popHdxq.show();
+			pgScroll[0].disable();
+			pgScroll[1].refresh();
+		});
+		$cache.popHdxqX.on('click', function(e) {
+			e.stopPropagation();
+			e.preventDefault();
+			$cache.pop.hide();
+			$cache.popHdxq.hide();
+			pgScroll[0].enable();
+		});
+		/*
+		$.getJSON("http://task.wangfan.com/task.ashx", function(data) {
+			console.log(data);
+			$cache.infoCent.append(data.jsonResponse.taskcontent).append('<img src="'+data.jsonResponse.taskbigimg+'" />');
+		});
+		*/
+	};
+	global[fun] = pgFun;
+})('fun', this);
 
-	/* trigger when page is ready */
-	$(document).ready(function (){
-	
-		// your functions go here
-	
+;(function($) {
+
+	$(document).ready(function() {
+
+		fun.index();
 	});
-	
-	
-	/* optional triggers
-	
-	$(window).load(function() {
-		
-	});
-	
-	$(window).resize(function() {
-		
-	});
-	
-	*/
 
 })(window.jQuery);
